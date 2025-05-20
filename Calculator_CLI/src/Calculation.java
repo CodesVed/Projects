fe08ea6c49c7174fa851f5964da65dedcae1559e
CodeVed04 <ved04.dev@gmail.com>
2025-05-20 23:11:50 +0530

    Improved UI: added color scheme, additional calculator features added; Completion of Calculator Project

--- Calculator_CLI/src/Calculation.java
+++ Calculator_CLI/src/Calculation.java
@@ -1,74 +1,101 @@
-import java.lang.reflect.Method;
 import java.math.BigInteger;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Calculation {
-    private static final Scanner sc = new Scanner(System.in);
+public final class Calculation {
+    public Calculation() {}
 
-    public static void add() throws InterruptedException {
-        System.out.print("Enter count of numbers: ");
-        int count = sc.nextInt();
-        double[] nums = Methods.inputArray(count);
-
+    public static double add(Scanner sc) {
         double sum = 0;
-        Methods.animatedDots("\nAdding");
-        Thread.sleep(500);
-        for (int i=0; i<count; i++){
-            sum += nums[i];
+
+        try {
+            System.out.print(ConsoleColors.WHITE_BOLD()+"Enter count of numbers: "+ConsoleColors.RESET());
+            int count = sc.nextInt();
+
+            for (int i = 1; i <= count; i++) {
+                System.out.print(ConsoleColors.WHITE_BOLD()+ "Enter number " + i + ": "+ConsoleColors.RESET());
+                sum += sc.nextDouble();
+            }
+
+            UIUtil.showLoading(ConsoleColors.YELLOW()+"\nAdding"+ConsoleColors.RESET());
+            return sum;
+
+        } catch (Exception e) {
+            System.out.println(ConsoleColors.RED_BOLD() + "ERROR: Invalid input!" + ConsoleColors.RESET());
+            sc.nextLine();
         }
-
-        System.out.printf("Result: %.2f%n", sum);
-        Thread.sleep(500);
-
-        sc.close();
+        return sum;
     }
 
-    public static void subtract(double a, double b){
-        double result = a-b;
-        Methods.animatedDots("\nSubtracting");
-        System.out.printf("Result: %.2f%n", result);
-        Methods.pause(500);
+
+    public static double subtract(double a, double b){
+        double diff = a-b;
+        UIUtil.showLoading(ConsoleColors.YELLOW()+"\nSubtracting"+ConsoleColors.RESET());
+        return diff;
     }
 
-    public static void multiply(double a, double b){
+    public static double multiply(double a, double b){
         double result = a*b;
-        Methods.animatedDots("\nMultiplying");
-        System.out.printf("Result: %.2f%n", result);
-        Methods.pause(500);
+        UIUtil.showLoading(ConsoleColors.YELLOW()+"\nMultiplying"+ConsoleColors.RESET());
+        return result;
     }
 
-    public static void divide(double a, double b){
+    public static double divide(double a, double b){
         double result = a/b;
-        Methods.animatedDots("\nDividing");
-        System.out.printf("Result: %.2f%n", result);
-        Methods.pause(500);
+        UIUtil.showLoading(ConsoleColors.YELLOW()+"\nDividing"+ConsoleColors.RESET());
+        return result;
     }
 
-    public static void exponent(double base, double power){
+    public static double exponent(double base, double power){
         double result = 1;
 
-        Methods.animatedDots("\nCalculating");
+        UIUtil.showLoading(ConsoleColors.YELLOW()+"\nCalculating"+ConsoleColors.RESET());
         for (int i=0; i<power; i++){
             result *= base;
         }
 
-        System.out.printf("%.2f raised to the power %.2f is: %.2f%n", base,power,result);
-        Methods.pause(500);
+        return result;
     }
 
-    public static void percentOf(double percent, double value){
-        Methods.animatedDots("\nCalculating");
-        double result = (percent/100)*value;
+    public static double percentOf(double percent, double value){
+        UIUtil.showLoading(ConsoleColors.YELLOW()+"\nCalculating"+ConsoleColors.RESET());
 
-        System.out.printf("%.2f%% of %.2f is %.2f%n", percent,value,result);
-        Methods.pause(500);
+        return (percent/100)*value;
     }
 
     public static BigInteger factorial(int n){
+        if (n<0) throw new IllegalArgumentException(ConsoleColors.RED()+"n should be >= 0"+ConsoleColors.RESET());
+
         if (n==0){
             return BigInteger.ONE;
         }
-
         return BigInteger.valueOf(n).multiply(factorial(n-1));
+    }
+
+    public static double compInterest(Scanner sc){
+        System.out.print(ConsoleColors.WHITE_BOLD()+"Enter principal: "+ConsoleColors.RESET());
+        double p = sc.nextDouble();
+        System.out.print(ConsoleColors.WHITE_BOLD()+"Enter rate of interest: "+ConsoleColors.RESET());
+        double r = sc.nextDouble();
+        System.out.print(ConsoleColors.WHITE_BOLD()+"Enter no. of times interest compounded: "+ConsoleColors.RESET());
+        int n = sc.nextInt();
+        System.out.print(ConsoleColors.WHITE_BOLD()+"Enter time[in year(s)]: "+ConsoleColors.RESET());
+        int t = sc.nextInt();
+
+        return p + exponent((1+(r/n)), (n*t));
+    }
+
+    public static double simpInterest(Scanner sc){
+        System.out.print(ConsoleColors.WHITE_BOLD()+"Enter principal: "+ConsoleColors.RESET());
+        double p = sc.nextDouble();
+        System.out.print(ConsoleColors.WHITE_BOLD()+"Enter rate of interest: "+ConsoleColors.RESET());
+        double r = sc.nextDouble();
+        System.out.print(ConsoleColors.WHITE_BOLD()+"Enter time[in year(s)]: "+ConsoleColors.RESET());
+        int t = sc.nextInt();
+
+        double a = p*r*t;
+
+        UIUtil.showLoading(ConsoleColors.YELLOW()+"\nCalculating"+ConsoleColors.YELLOW());
+        return a;
     }
 }
